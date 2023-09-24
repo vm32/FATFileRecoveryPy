fb444600dd2eba68af345700820b9db8e45dbc2f
Abdullah <21219411+vm32@users.noreply.github.com>
2023-09-24 13:23:57 +0300

    1337

--- recovery.py
+++ recovery.py
@@ -1,11 +1,8 @@
 import os
 import struct
-
 image_path = "path_to_your_fat_image.img"
-
 # Open the FAT image in binary read mode
 with open(image_path, "rb") as image:
-
     boot_sector = image.read(512)
     bytes_per_sector = struct.unpack("<H", boot_sector[11:13])[0]
     sectors_per_cluster = boot_sector[13]
@@ -16,16 +13,13 @@ with open(image_path, "rb") as image:
     sectors_per_fat = struct.unpack("<H", boot_sector[22:24])[0]
     root_dir_start = (reserved_sectors + number_of_fats * sectors_per_fat) * bytes_per_sector
     data_region_start = root_dir_start + (root_entries * 32)
-
     image.seek(root_dir_start)
-
     for i in range(root_entries):
         entry = image.read(32)
         if entry[0] == 0xE5:  # Check if the file is marked as deleted
             file_name = entry[:8].decode('ascii').strip()
             file_ext = entry[8:11].decode('ascii').strip()
             file_size = struct.unpack("<I", entry[28:32])[0]
-
             starting_cluster = struct.unpack("<H", entry[26:28])[0]
             file_data_address = data_region_start + (starting_cluster - 2) * sectors_per_cluster * bytes_per_sector
             image.seek(file_data_address)
